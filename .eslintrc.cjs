ecd4a50377f62bcd92d1a75e064275373d97965e
Ze Zan Ziet <vietvan05032002@gmail.com>
2024-04-23 00:08:01 +0700

    installation eslint eslint-typescript eslint-config-airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
-
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <>
       Hello world
     </>
-  )
+  );
 }
 
-export default App
+export default App;
